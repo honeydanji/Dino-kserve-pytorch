982af21d43cdb12a5436f964b0c0316515e636a8
하성진 <hsj@eumservice.com>
2024-08-19 17:29:35 +0900

    # create model pipeline with kfp

---
+++ kfp/model_pipeline.py
@@ -0,0 +1,34 @@
+from kfp import dsl
+import boto3
+import os
+
+
+def data_download_from_s3():
+    pass
+
+def data_prepare():
+    pass
+
+
+def data_split():
+    pass
+
+
+def model_train():
+    pass
+
+
+def model_validation():
+    pass
+
+
+def model_deploy():
+    pass
+
+
+def main():
+    data_download_from_s3()
+
+
+if __name__ == "__main__":
+    main()
