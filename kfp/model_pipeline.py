828b7b9e1a94bcf28c94c27dad91bc531be952fe
하성진 <hsj@eumservice.com>
2024-08-26 13:22:01 +0900

    # add logging in component of kfp

--- kfp/model_pipeline.py
+++ kfp/model_pipeline.py
@@ -2,6 +2,7 @@ import json
 
 from kfp.dsl import component, Input, Output, Artifact, Dataset
 
+
 @component(
     packages_to_install=["boto3"],
     base_image="python:3.9"
@@ -21,6 +22,7 @@ def data_download_from_s3(output_data: Output[Artifact]):
     start_after = 'kfp/images/'
     objects = s3_kfp_client.list_objects_v2(Bucket=bucket_name, StartAfter=start_after)['Contents']
 
+    ## 모든 이미지를 호출하는 게 아니라 지정한 데이터 수에 도달한 공종만 호출해야 한다. (이후 수정이 필요함)
     images_path = {
         "wall": [],
         "tile": [],
@@ -28,12 +30,13 @@ def data_download_from_s3(output_data: Output[Artifact]):
         "etc": []
     }
 
+
     for obj in objects:
         key = obj['Key']
         types = key.split('/')[-2]
 
         if types not in images_path.keys():
-            raise Exception(f"{types} 공종이 존재 하지 않음.")
+            raise ValueError(f"{types} 공종이 존재 하지 않음.")
 
         if key.endswith(('.jpg', '.jpeg', '.png')):
             image_url = f'https://{bucket_name}.s3.ap-northeast-2.amazonaws.com/{key}'
@@ -55,9 +58,12 @@ def data_download_from_s3(output_data: Output[Artifact]):
     packages_to_install=["pandas"],
     base_image="python:3.9"
 )
-def data_prepare(input_data: Input(Artifact), output_data: Output[Dataset]):
+def data_preprocessing(input_data: Input(Artifact), output_data: Output[Dataset]):
     import pandas as pd
+    import logging
     import json
+
+    logging.basicConfig(level=logging.INFO)
 
     with open(input_data.path, 'r') as f:
         images_path = json.load(f)
@@ -70,20 +76,37 @@ def data_prepare(input_data: Input(Artifact), output_data: Output[Dataset]):
         })
         data = pd.concat([data, temp_df], ignore_index=True)
 
+    if data is None:
+        logging.error("데이터가 존재 하지 않음.")
+        raise ValueError("데이터 존재 하지 않음.")
+
+    ## 데이터셋 라벨 종류 및 수 확인 - 공종 별 이미지 수 조건 걸기 가능.
+    label_counts = data['label'].value_counts()
+    for label, count in label_counts.items():
+        print(f"{label}: {count}")
+        logging.info(f"{label}: {count}")
+
+
+    ## 데이터 csv 형식으로 저장
     data.to_csv(output_data.path, index=False)
 
 
-def data_split(input_data: Input(Dataset)):
+@component(
+
+)
+def data_split_and_post_processing(input_data: Input(Dataset)):
     print(input_data)
 
 
 def model_train():
     pass
 
+
 def model_validation():
     pass
 
-def model_deploy():
+
+def model_update_to_():
     pass
 
 
